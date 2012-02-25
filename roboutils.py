b93cf5c938bb9f045d6ca343ff1f377602131e85
Pete Marshall <petemarshall77@gmail.com>
2012-02-24 21:28:05 -0800

    Support Windows get_local_ipv4_address()

--- roboutils.py
+++ roboutils.py
@@ -3,16 +3,18 @@
 # NOTE - everything in this module is imported (from roboutils import *), so
 #        don't add anything here unless it truly is a shared utility function
 #        or object. Let's keep that namespace clean, folks!
-import commands,re
+import commands,re,os,socket
 
 # Get the IPv4 address of the current computer
-# NOTE: Linux only at this point
 def get_local_ipv4_address():
     
-    output = commands.getoutput("/sbin/ifconfig | grep inet | grep -v inet6 | grep -v 127.0.0.1")
-    ip_re = re.compile('inet addr:(\S+)')
-    m = ip_re.search(output)
-    return m.group(1)
+    if os.name == 'posix':
+        output = commands.getoutput("/sbin/ifconfig | grep inet | grep -v inet6 | grep -v 127.0.0.1")
+        ip_re = re.compile('inet addr:(\S+)')
+        m = ip_re.search(output)
+        return m.group(1)
+    elif os.name == 'nt':
+        return(socket.gethostbyname(socket.gethostname()))
 
 class RobotError(Exception):
     def __init__(self, value):
