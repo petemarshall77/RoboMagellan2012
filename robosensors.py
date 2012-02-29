bd0512f1a2febc03310a107438cc30b9bd3bc815
Pete Marshall <petemarshall77@gmail.com>
2012-02-28 20:14:48 -0800

    Added touch and Ping sensor interface - corrected commit

--- robosensors.py
+++ robosensors.py
@@ -7,8 +7,12 @@ from roboutils import *
 
 # Sensor values
 compass_value = 999
+bumper_status = False
+ping_data = [999,999,999,999]
 
+#------------------------------------------------------------------------
 # Compass communication thread
+#------------------------------------------------------------------------
 class CompassThread(threading.Thread):
 
     ser = serial.Serial(roboconfig.compass_serial_port-1, \
@@ -33,22 +37,76 @@ class CompassThread(threading.Thread):
     def stop(self):
         self.run_flag = False
 
+
+#------------------------------------------------------------------------
+# PING and bumper communication thread
+#------------------------------------------------------------------------
+class SensorThread(threading.Thread):
+
+    ser = serial.Serial(roboconfig.sensor_serial_port-1, \
+                            roboconfig.sensor_serial_baud)
+    sensor_re = re.compile('sensor (\d): (\d+)')
+    run_flag = True
+
+    # Get the sensor data
+    def run(self):
+        while self.run_flag == True:
+            global ping_data, bumper_status
+            self.ser.flushInput() # Discard readings buffered during sleep
+            self.ser.readline()
+            # Read five lines of data to get complete input
+            for i in range(5):
+                data = self.ser.readline().lower().strip()
+                if data.find('bumper') == 0:
+                    if data.find('on') > 0:
+                        bumper_status = True
+                    elif data.find('off') > 0:
+                        bumper_status = False
+                    else:
+                        log("robosensors - recieved invalid bumper data: %s" % \
+                                data)
+                elif data.find('sensor') == 0:
+                    m = self.sensor_re.match(data)
+                    if m:
+                        ping_data[int(m.group(1))-1] = int(m.group(2))
+                    else:
+                        log("robosensors - recieved invalid sensor data: %s" % \
+                                data)
+                else:
+                    log("robosensor - received invalid data %s" % data)
+
+            time.sleep(roboconfig.sensor_read_delay)
+
+    
+    # Stop the thread
+    def stop(self):
+        self.run_flag = False
+
+
 def initialize():
     log("Sensor initialization started")
     compass_thread = CompassThread()
     compass_thread.start()
+    sensor_thread = SensorThread()
+    sensor_thread.start()
     log("Sensor initialization completed")
     return True
 
-# get_compass() - get the compass data
-#
-#    Returns the compass heading - based on true (not magnetic) north -
-#    as an integer between 0 and 359, where 0 is north, 90 is east, 180
-#    is south, and 270 is west
+#-----------------------------------------------------------------------
+# Return the sensor data
+#-----------------------------------------------------------------------
 def get_compass():
     global compass_data
     return compass_data
 
+def get_bumper():
+    global bumper_status
+    return bumper_status
+
+def get_sensors():
+    global ping_data
+    return ping_data
+
 # touch_down() - return True if the touch sensor is activated
 def touch_down():
-    return True
+    return get_bumper()
